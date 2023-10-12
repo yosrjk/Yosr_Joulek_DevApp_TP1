34254296a9e8f6ad768822545c4ed02b96d467d8
yosr <joulekyosr123@gmail.com>
2023-10-12 18:03:58 +0100

    second commit

--- src/ClientPackage/Client.java
+++ src/ClientPackage/Client.java
@@ -5,20 +5,30 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.Scanner;
 public class Client {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Je suis un client pas encore connecté");
-        int serverPort = 1234; 
-        try {
-        	
-            InetAddress host = InetAddress.getByName("localhost");
-            Socket socket = new Socket(host, 1234); 
-            System.out.println("Je suis un client connecté");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    
+		try {
+			Socket socket=new Socket("localhost",1238);
+			System.out.println("un client est connecté ");	
+		    Scanner scanner =  new Scanner(System.in);
+		    
+			System.out.println("Donner un nb " );
+		    int x=scanner.nextInt();
+		  
+		    OutputStream os=socket.getOutputStream();
+		    os.write(x); 
+		
+		    InputStream is= socket.getInputStream();
+		    int nb = is.read();
+			System.out.println("La réponse est " +nb);
+		    socket.close();
+	
+		  }catch (IOException e){
+			e.printStackTrace();
+		}
+	}
 
-        System.out.println("Je suis un client connecté");
-    }
 }
--- src/ServerPackage/Server.java
+++ src/ServerPackage/Server.java
@@ -7,22 +7,28 @@ import java.net.Socket;
 
 public class Server {
 
-	public static void main(String[] args) throws IOException{
-		int serverPort=1234;
-		ServerSocket ss=new ServerSocket(serverPort);
-		// TODO Auto-generated method stub
-      System.out.println("je suis un serveur en attente la connexion d'un client");
-      try {
-      Socket socket=ss.accept();
-      System.out.println("un client est connecte");
-      socket.close();
-	 } 
-	catch (Exception e) {e.printStackTrace();}
-
-	}
-    
-
+	public static void main(String[] args) {
+		try {
+			ServerSocket ss=new ServerSocket(1238);
+			System.out.println("Je suis un serveur en attente la connexion d'un client ");
+		    Socket s=ss.accept();
+		    System.out.println("Un client connecté ");
+		    InputStream is= s.getInputStream();
+		     OutputStream os=s.getOutputStream();
+			System.out.println("J'attend un nombre x = ");
+		    int x=is.read();
+		  
+		    int nb=x*5;
+			System.out.println("La réponse: ");	
+			
+			os.write(nb);
+			//La derniére étape: Fermer socket
+			s.close();
+		  
 	
+		  }catch (IOException e){
+			e.printStackTrace();
+		}
+	}
 }
-
 
